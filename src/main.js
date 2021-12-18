922fd94ad7c19e9563641264579f29d1213f0c71
fcesc-code <bpfran@gmail.com>
2021-12-18 16:05:01 +0100

    config / add index.html and main.js files

---
+++ src/main.js
@@ -0,0 +1,15 @@
+/* [ STYLES ] */
+
+/* card component */
+// const cards = document.querySelectorAll(".card__content");
+
+// if (cards) {
+//   cards.forEach((card) => {
+//     card.addEventListener("click", (event) => {
+//       event.currentTarget.classList.toggle("flipped");
+//     });
+//   });
+// }
+
+/* [ SCRIPTS ] */
+console.warn("TEST: Welcome back to your boilerplate!");
